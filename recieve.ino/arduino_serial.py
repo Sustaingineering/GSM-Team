35b6dfd7e328713dcc0fa8bac06f0e37ab72a7f7
Eli Carlin-Coleman <eli.carlin-coleman@alumni.ubc.ca>
2018-09-07 23:18:34 -0700

    Successfully write to JSON file

--- recieve.ino/arduino_serial.py
+++ recieve.ino/arduino_serial.py
@@ -1,5 +1,6 @@
 import serial
 import csv
+import json
 import re
 import matplotlib.pyplot as plt
 # import pandas as pd
@@ -7,13 +8,15 @@ import matplotlib.pyplot as plt
 portPath = "COM1"	   # Must match value shown on Arduino IDE
 baud = 115200					 # Must match Arduino baud rate
 timeout = 999					  # Seconds
-filename = "data.csv"
+c_filename = "data.csv"
+j_filename = "data.json"
 max_num_readings = 999
 num_signals = 1
 
 sms=" "
  
-Fields = ["Load Voltage: ", "Load Current: ", "Power: ", "Atmospheric Temperature: ", "Solar Panel Temperature: ", "Water Breaker Flag: "]
+# Fields = ["Load Voltage: ", "Load Current: ", "Power: ", "Atmospheric Temperature: ", "Solar Panel Temperature: ", "Water Breaker Flag: "]
+Fields = ["Load Voltage", "Load Current", "Power", "Atmospheric Temperature", "Solar Panel Temperature", "Water Breaker Flag"]
  
 def create_serial_obj(portPath, baud_rate, tout):
 	"""
@@ -47,8 +50,9 @@ def read_serial_data(serial):
 		# print serial_line
 		sms=""
 		for s in Fields:
-			if serial_line.find(s)==0:
-				temp=""+s+serial_line.partition(": ")[2]
+			if serial_line.find(s+": ")==0:
+				serial_data.append(serial_line.partition(": ")[2])
+				temp=""+s+": "+serial_data[-1]
 				print temp
 				sms=sms+temp+","
 		if serial_line == '':
@@ -58,8 +62,8 @@ def read_serial_data(serial):
 			if len(serial_data) == max_num_readings:
 				readings_left = False
 	print sms
-		
-	return serial_data
+	
+	
  
 def is_number(string):
 	"""
@@ -89,14 +93,37 @@ def clean_serial_data(data):
  
 	return clean_data		   
  
-def save_to_csv(data, filename):
+def save_to_csv(data, c_filename):
 	"""
-	Saves a list of lists (data) to filename
+	Saves a list of lists (data) to filename.csv
 	"""
-	with open(filename, 'wb') as csvfile:
+	# Broken
+	with open(c_filename, 'wb') as csvfile:
+		csvread = csv.reader(csvfile)
+		for row in csvread:
+			row_array.append(row)
+		
 		csvwrite = csv.writer(csvfile)
-		csvwrite.writerows(data)
+		
+		csvwrite.writerow(data)
  
+def save_to_json(data, j_filename):
+	"""
+	Saves a list of lists (data) to filename.json
+	"""
+	fr=open(j_filename,"r")
+	jsoncontent=fr.read()
+	fr.close()
+	fw=open(j_filename, "w")
+	jsondata="{\n"
+	for i in range(len(Fields)):
+		jsondata+="\t"
+		jsondata+="\""+Fields[i]+"\": "
+		jsondata+="\""+str(data[i])+"\""
+		jsondata+=",\n"
+	jsondata+="}\n"
+	fw.write(jsoncontent+jsondata)
+	
 def gen_col_list(num_signals):
 	"""
 	Given the number of signals returns
@@ -120,38 +147,44 @@ def simple_plot(csv_file, columns, headers):
 	plt.plotfile(csv_file, columns, names=headers, newfig=True)
 	plt.show()
  
-def plot_csv(csv_file, cols):
-	# Create Pandas DataFrame from csv data
-	data_frame = pd.read_csv(csv_file)
-	# Set the names of the columns
-	data_frame.columns = cols
-	# Set the first column (Time) as the index 
-	data_frame = data_frame.set_index(cols[0])
-	# Map the voltage values from 0-1023 to 0-5
-	data_frame = data_frame.apply(lambda x: map_value(x,0.,1023,0,5))
-	# Bring back the Time column
-	data_frame = data_frame.reset_index()
-	plt.clf()
-	plt.close()
-	# Plot the data
-	data_frame.plot(x=cols[0],y=cols[1:])
-	plt.show()
+# def plot_csv(csv_file, cols):
+	# # Create Pandas DataFrame from csv data
+	# data_frame = pd.read_csv(csv_file)
+	# # Set the names of the columns
+	# data_frame.columns = cols
+	# # Set the first column (Time) as the index 
+	# data_frame = data_frame.set_index(cols[0])
+	# # Map the voltage values from 0-1023 to 0-5
+	# data_frame = data_frame.apply(lambda x: map_value(x,0.,1023,0,5))
+	# # Bring back the Time column
+	# data_frame = data_frame.reset_index()
+	# plt.clf()
+	# plt.close()
+	# # Plot the data
+	# data_frame.plot(x=cols[0],y=cols[1:])
+	# plt.show()
 	
-	
+
 print "Creating serial object..."
 serial_obj = create_serial_obj(portPath, baud, timeout)
+
+while 1:
+
+	print "Reading serial data..."
+	read_serial_data(serial_obj)
+	print "Length: "+len(serial_data)
+
+	print "Cleaning data..."
+	clean_data =  clean_serial_data(serial_data)
+
+	clean_data=[1,2,3,4,5,6]
+	# print "Saving to csv..."
+	# save_to_csv(clean_data, c_filename)
+
+	print "Saving to json..."
+	save_to_json(clean_data, j_filename)
  
-print "Reading serial data..."
-serial_data = read_serial_data(serial_obj)
-print "Length: "+len(serial_data)
- 
-print "Cleaning data..."
-clean_data =  clean_serial_data(serial_data)
- 
-print "Saving to csv..."
-save_to_csv(clean_data, filename)
- 
-print "Plotting data..."
-#simple_plot(filename, (0,1,2), ['time (s)', 'voltage1', 'voltage2'])
-#simple_plot(filename, (0,1), ['time (s)', 'voltage1'])
-plot_csv(filename, gen_col_list(num_signals))
+# print "Plotting data..."
+# #simple_plot(filename, (0,1,2), ['time (s)', 'voltage1', 'voltage2'])
+# #simple_plot(filename, (0,1), ['time (s)', 'voltage1'])
+# plot_csv(filename, gen_col_list(num_signals))
